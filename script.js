01478e10efecef43256cff5981c6c7194c2ec2e7
k-vasanth09072002 <vasanthvasath74@gmail.com>
2024-03-23 15:54:57 +0530

    math.floor-math.ceil

---
+++ script.js
@@ -0,0 +1,23 @@
+// math floor
+
+var a=Math.floor(5.7)
+var b=Math.floor(0.70)
+var c=Math.floor(-1.2)
+var d=Math.floor(-.9)
+
+
+
+// Math.ceil
+
+var e=Math.ceil(8.1)
+var f=Math.floor(0.70)
+var g=Math.floor(-1.2)
+var h=Math.floor(-.9)
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
